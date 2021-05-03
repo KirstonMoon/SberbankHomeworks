cf885269ef809c1b378f4ef2b657276633ed6e97
Kirill Magerya <fresno.super@gmail.com>
2021-05-03 16:12:15 +0700

    добавил папку с 6 дз
    добавил выполненный 1 файл из дз

---
+++ SberbankHomeworks/Lection 6/SwiftGenerics_01.playground/Contents.swift
@@ -0,0 +1,31 @@
+import UIKit
+
+func sumTwoValues<T: AdditiveArithmetic>(_ a: T, _ b: T) -> T {
+	let result = a + b
+	return result
+}
+
+let a = 25.0
+let b = 34.0
+
+let resultDouble = sumTwoValues(a, b)
+print(resultDouble)
+
+let c = "ABC"
+let d = "DEF"
+
+let resultString = sumTwoValues(c, d)
+print(resultString)
+
+
+extension String: AdditiveArithmetic {
+    public static func - (lhs: String, rhs: String) -> String {
+        ""
+    }
+    
+    public static var zero: String {
+        ""
+    }
+}
+
+
