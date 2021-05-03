2f30646ccd28b40f56a6cd9461f99c3168fca1fa
Kirill Magerya <fresno.super@gmail.com>
2021-05-03 17:30:24 +0700

    добавил выполненный 2 файл

--- SberbankHomeworks/Lection 6/SwiftGenerics_01.playground/Contents.swift
+++ SberbankHomeworks/Lection 6/SwiftGenerics_01.playground/Contents.swift
@@ -29,3 +29,4 @@ extension String: AdditiveArithmetic {
 }
 
 
+
--- SberbankHomeworks/Lection 6/SwiftGenerics_02.playground/Contents.swift
+++ SberbankHomeworks/Lection 6/SwiftGenerics_02.playground/Contents.swift
@@ -1,3 +1,51 @@
 import UIKit
 
 // MARK: - Задача 2
+
+protocol Container {
+    associatedtype Element
+    
+    var elements: [Element] { get set }
+    var count: Int { get }
+    mutating func append(element: Element)
+    
+    subscript(element: Int) -> Element { get }
+}
+
+extension Container {
+    var count: Int {
+        elements.count
+    }
+    
+    mutating func append(element: Element) {
+        elements.append(element)
+    }
+    
+    subscript(element: Int) -> Element {
+        elements[element]
+    }
+}
+
+struct Stack<T>: Container {
+    typealias Element = T
+    
+    var elements = [T]()
+    
+    mutating func pop(element: T) -> T? {
+        elements.popLast()
+    }
+}
+
+struct Queue<T>: Container {
+    typealias Element = T
+    
+    var elements = [T]()
+    
+    mutating func dequeue() -> T? {
+        if elements.isEmpty {
+            return nil
+        }
+        return elements.removeFirst()
+    }
+}
+
