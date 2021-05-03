e3f35ca3be93a7542af3d4b585bf75df0a0fbf23
Kirill Magerya <fresno.super@gmail.com>
2021-05-03 17:43:31 +0700

    добавил выполненный 3 файл

--- SberbankHomeworks/Lection 6/SwiftGenerics_03.playground/Contents.swift
+++ SberbankHomeworks/Lection 6/SwiftGenerics_03.playground/Contents.swift
@@ -2,3 +2,11 @@ import UIKit
 
 // MARK: - Задача 3*. К выполнению необязательна. 
 
+enum LinkedList<T> {
+    indirect case Cons(T, LinkedList<T>)
+    case Nil
+}
+
+let list = LinkedList.Cons(1, .Cons(2, .Cons(3, .Nil)))
+
+
