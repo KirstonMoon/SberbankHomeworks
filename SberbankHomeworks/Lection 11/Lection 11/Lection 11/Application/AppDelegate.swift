88476b7514f534e29610b5ffbec76a050bff52ab
Kirill Magerya <fresno.super@gmail.com>
2021-05-19 15:49:42 +0700

    начал выполнение 11 дз

---
+++ SberbankHomeworks/Lection 11/Lection 11/Lection 11/Application/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  Lection 11
+//
+//  Created by Kirill Magerya on 19.05.2021.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        window = UIWindow(frame: UIScreen.main.bounds)
+        
+        let viewController = ViewController()
+        
+        window?.rootViewController = viewController
+        window?.makeKeyAndVisible()
+        
+        return true
+    }
+}
+
---
+++ SberbankHomeworks/Lection 11/Lection 11/Lection 11/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Lection 11
+//
+//  Created by Kirill Magerya on 19.05.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .blue
+    }
+
+
+}
+
