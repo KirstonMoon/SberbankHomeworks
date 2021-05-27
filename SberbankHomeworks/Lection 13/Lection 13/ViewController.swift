3ce75c010dbf0b6bde722b3f84bf34f96f15f102
Kirill Magerya <fresno.super@gmail.com>
2021-05-28 00:08:14 +0700

    переписал код

--- SberbankHomeworks/Lection 13/Lection 13/ViewController.swift
+++ SberbankHomeworks/Lection 13/Lection 13/ViewController.swift
@@ -9,190 +9,87 @@ import UIKit
 
 final class ViewController: UIViewController {
     
-    private lazy var animateButton: UIButton = {
-        let button = UIButton()
-        button.translatesAutoresizingMaskIntoConstraints = false
-        button.setTitle("Start", for: .normal)
-        button.backgroundColor = .systemBlue
-        button.layer.cornerRadius = 25
-        button.addTarget(self, action: #selector(buttonTapped), for: .touchUpInside)
-        return button
-    }()
-    
     private lazy var treePicture: UIImageView = {
         let tree = UIImageView()
-        tree.translatesAutoresizingMaskIntoConstraints = false
         tree.contentMode = .scaleAspectFit
         tree.image = UIImage(named: "TreePicture")
         tree.isHidden = true
         return tree
     }()
     
-    private lazy var appleOne: UIView = {
-        let apple = UIView()
-        apple.translatesAutoresizingMaskIntoConstraints = false
-        apple.isHidden = true
-        apple.layer.cornerRadius = 20
-        apple.backgroundColor = .systemRed
-        return apple
-    }()
-    
-    private lazy var appleTwo: UIView = {
-        let apple = UIView()
-        apple.translatesAutoresizingMaskIntoConstraints = false
-        apple.isHidden = true
-        apple.layer.cornerRadius = 20
-        apple.backgroundColor = .systemRed
-        return apple
-    }()
-    
-    private lazy var appleThree: UIView = {
-        let apple = UIView()
-        apple.translatesAutoresizingMaskIntoConstraints = false
-        apple.isHidden = true
-        apple.layer.cornerRadius = 20
-        apple.backgroundColor = .systemRed
-        return apple
-    }()
-    
-    private lazy var appleFour: UIView = {
-        let apple = UIView()
-        apple.translatesAutoresizingMaskIntoConstraints = false
-        apple.isHidden = true
-        apple.layer.cornerRadius = 20
-        apple.backgroundColor = .systemRed
-        return apple
-    }()
-    
-    private lazy var appleFive: UIView = {
-        let apple = UIView()
-        apple.translatesAutoresizingMaskIntoConstraints = false
-        apple.isHidden = true
-        apple.layer.cornerRadius = 20
-        apple.backgroundColor = .systemRed
-        return apple
+    private lazy var apples: [UIImageView] = {
+        var apples = [UIImageView]()
+        for _ in 1...8 { apples.append(UIImageView(image: UIImage(named: "ApplePicture"))) }
+        
+        apples.forEach { apple in
+            apple.contentMode = .scaleAspectFit
+            apple.isHidden = true
+        }
+        
+        return apples
     }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         view.backgroundColor = .systemBackground
-        addingSubviews()
-        activateConstraints()
+        
+        view.addSubview(treePicture)
+        apples.forEach { view.addSubview($0) }
+        
+        apples.forEach { $0.frame.size = .init(width: 0, height: 0) }
+        apples.forEach { $0.center.x = CGFloat(Int.random(in: Int(view.frame.midX - 100)...Int(view.frame.midX + 100))) }
+        apples.forEach { $0.center.y = CGFloat(Int.random(in: Int(view.frame.midY - 110)...Int(view.frame.midY - 50))) }
+        
+        treePicture.frame.size = .init(width: 0, height: 0)
+        treePicture.center.x = view.frame.midX
+        treePicture.center.y = view.frame.midY
+        
+        startAnimation()
     }
     
-    @objc private func buttonTapped() {
+    private func startAnimation() {
+        
+        treeGrowingAnimation()
+        DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
+            self.appleAnimation()
+        }
+    }
+    
+    private func treeGrowingAnimation() {
         
         treePicture.isHidden = false
-        
         treePicture.layer.removeAllAnimations()
-        appleOne.layer.removeAllAnimations()
-        appleTwo.layer.removeAllAnimations()
-        appleThree.layer.removeAllAnimations()
-        appleFour.layer.removeAllAnimations()
-        appleFive.layer.removeAllAnimations()
         
-        treeStartGrowingAnimation()
-        
-        DispatchQueue.main.asyncAfter(deadline: .now() + 4) { [weak self] in
-            self?.appleOne.isHidden = false
-            self?.appleTwo.isHidden = false
-            self?.appleThree.isHidden = false
-            self?.appleFour.isHidden = false
-            self?.appleFive.isHidden = false
-            self?.appleStartOpacityAimation()
-        }
-    }
-}
-
-// MARK: Setup subviews
-
-private extension ViewController {
-    
-    func addingSubviews() {
-        view.addSubview(treePicture)
-        view.addSubview(animateButton)
-        treePicture.addSubview(appleOne)
-        treePicture.addSubview(appleTwo)
-        treePicture.addSubview(appleThree)
-        treePicture.addSubview(appleFour)
-        treePicture.addSubview(appleFive)
-    }
-}
-
-// MARK: Setup constraints
-
-private extension ViewController {
-    
-    func activateConstraints() {
-        
-        NSLayoutConstraint.activate([
-            animateButton.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 30),
-            animateButton.centerXAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerXAnchor),
-            animateButton.widthAnchor.constraint(equalToConstant: 100),
-            animateButton.heightAnchor.constraint(equalToConstant: 50),
-            
-            treePicture.topAnchor.constraint(equalTo: animateButton.bottomAnchor),
-            treePicture.leftAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leftAnchor),
-            treePicture.rightAnchor.constraint(equalTo: view.safeAreaLayoutGuide.rightAnchor),
-            treePicture.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
-            
-            appleOne.heightAnchor.constraint(equalToConstant: 40),
-            appleOne.widthAnchor.constraint(equalToConstant: 40),
-            appleOne.centerXAnchor.constraint(equalTo: treePicture.centerXAnchor, constant: 20),
-            appleOne.centerYAnchor.constraint(equalTo: treePicture.centerYAnchor, constant: -20),
-            
-            appleTwo.heightAnchor.constraint(equalToConstant: 40),
-            appleTwo.widthAnchor.constraint(equalToConstant: 40),
-            appleTwo.centerXAnchor.constraint(equalTo: treePicture.centerXAnchor, constant: 100),
-            appleTwo.centerYAnchor.constraint(equalTo: treePicture.centerYAnchor, constant: -50),
-            
-            appleThree.heightAnchor.constraint(equalToConstant: 40),
-            appleThree.widthAnchor.constraint(equalToConstant: 40),
-            appleThree.centerXAnchor.constraint(equalTo: treePicture.centerXAnchor, constant: -45),
-            appleThree.centerYAnchor.constraint(equalTo: treePicture.centerYAnchor, constant: -90),
-            
-            appleFour.heightAnchor.constraint(equalToConstant: 40),
-            appleFour.widthAnchor.constraint(equalToConstant: 40),
-            appleFour.centerXAnchor.constraint(equalTo: treePicture.centerXAnchor, constant: -105),
-            appleFour.centerYAnchor.constraint(equalTo: treePicture.centerYAnchor, constant: -65),
-            
-            appleFive.heightAnchor.constraint(equalToConstant: 40),
-            appleFive.widthAnchor.constraint(equalToConstant: 40),
-            appleFive.centerXAnchor.constraint(equalTo: treePicture.centerXAnchor, constant: 45),
-            appleFive.centerYAnchor.constraint(equalTo: treePicture.centerYAnchor, constant: -120)
-        ])
-    }
-}
-
-// MARK: Setup Tree animations
-
-private extension ViewController {
-    
-    func treeStartGrowingAnimation() {
         let options: UIView.AnimationOptions = [.curveEaseInOut]
-        UIView.animate(withDuration: 4, delay: 0, options: options) { [weak self] in
+        UIView.animate(withDuration: 3, delay: 0, options: options) {
+            [weak self] in
             guard let self = self else { return }
-            self.treePicture.frame.size.height *= 2
+            self.treePicture.bounds.size.height = self.view.frame.height
+            self.treePicture.bounds.size.width = self.view.frame.width
         }
     }
-}
-
-// MARK: Setup Apple animations
-
-private extension ViewController {
     
-    func appleStartOpacityAimation() {
+    private func appleAnimation() {
+        
+        apples.forEach { $0.layer.removeAllAnimations() }
+        
+        let options: UIView.AnimationOptions = [.curveEaseInOut]
         let animation = CABasicAnimation(keyPath: "opacity")
+        
         animation.fromValue = 0
         animation.toValue = 1
-        animation.duration = 2
+        animation.duration = 3
         animation.repeatCount = 1
         
-        appleOne.layer.add(animation, forKey: "opacityAppleOne")
-        appleTwo.layer.add(animation, forKey: "opacityAppleTwo")
-        appleThree.layer.add(animation, forKey: "opacityAppleThree")
-        appleFour.layer.add(animation, forKey: "opacityAppleFour")
-        appleFive.layer.add(animation, forKey: "opacityAppleFive")
+        UIView.animate(withDuration: 3, delay: 0, options: options) {
+            [weak self] in
+            guard let self = self else { return }
+            self.apples.forEach { $0.bounds.size.height = 50 }
+            self.apples.forEach { $0.bounds.size.width = 50 }
+        }
+        
+        apples.forEach { $0.isHidden = false }
+        apples.forEach { $0.layer.add(animation, forKey: "appleOpacity") }
     }
 }
