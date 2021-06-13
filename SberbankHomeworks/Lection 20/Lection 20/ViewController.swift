b18cd3ae5033e1abee7baeb8d7b3834ae97f7a24
Kirill Magerya <fresno.super@gmail.com>
2021-06-13 23:41:38 +0700

    add test framework app

---
+++ SberbankHomeworks/Lection 20/Lection 20/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  Lection 20
+//
+//  Created by Kirill Magerya on 13.06.2021.
+//
+
+import UIKit
+import TestFramework
+
+class ViewController: UIViewController {
+
+    let testFramework = TestFramework()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
